4c0f6d4c86fd37efe6a63ec00699830b3a763574
Justin Scott <justinjonscott@gmail.com>
2019-02-04 16:07:43 -0500

    Create User.py
    Start user file

---
+++ user/User.py
@@ -0,0 +1,27 @@
+class User:
+
+	user = {
+		'UUID':'',
+		'USERNAME':'',
+		'PASSWORD':'',
+		'FIRST_NAME':'',
+		'LAST_NAME':'',
+		'TIMESTAMP':'',
+		'ACCESS_LEVEL':''
+		}
+	
+	def __init__(self):
+	
+	# Create New User Object
+	@staticmethod
+	def new_user():
+	
+	# Delete passed User Object
+	@staticmethod
+	def delete_user():
+	
+	# Get a user using username or uuid
+	@staticmethod
+	def get_user(*args):
+	
+	
