7195131e194494424c10f13a87c6f96c94c9e172
Justin Scott <justinjonscott@gmail.com>
2019-02-05 12:14:06 -0500

    figuring out formatting for python

--- user/User.py
+++ user/User.py
@@ -1,27 +1,30 @@
 class User:
 
-	user = {
-		'UUID':'',
-		'USERNAME':'',
-		'PASSWORD':'',
-		'FIRST_NAME':'',
-		'LAST_NAME':'',
-		'TIMESTAMP':'',
-		'ACCESS_LEVEL':''
-		}
-	
-	def __init__(self):
-	
-	# Create New User Object
-	@staticmethod
-	def new_user():
-	
-	# Delete passed User Object
-	@staticmethod
-	def delete_user():
-	
-	# Get a user using username or uuid
-	@staticmethod
-	def get_user(*args):
-	
-	
+    user = {
+        'UUID':'',
+        'USERNAME':'',
+        'PASSWORD':'',
+        'FIRST_NAME':'',
+        'LAST_NAME':'',
+        'TIMESTAMP':'',
+        'ACCESS_LEVEL':''
+        }
+
+    def __init__(self):
+        pass
+
+    # Create New User Object
+    @staticmethod
+    def new_user():
+        pass
+
+    # Delete passed User Object
+    @staticmethod
+    def delete_user():
+        pass
+
+    # Get a user using username or uuid
+    @staticmethod
+    def get_user(*args):
+        pass
+
