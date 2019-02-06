76fd96d9b0993938b20a50d7f2be0388cb0152f3
Justin Scott <justinjonscott@gmail.com>
2019-02-06 13:40:42 -0500

    Settings config

---
+++ settings/config.py
@@ -0,0 +1,8 @@
+DEFAULT_LOCAL_PATH = "%appdata%/VisualSP2019/Users/"
+
+storage = dict(
+    path = DEFAULT_LOCAL_PATH,
+)
+
+
+
--- user/UserHelper.py
+++ user/UserHelper.py
@@ -1,5 +1,6 @@
 import json
 import os
+import settings.config
 
 
 class UserHelper:
@@ -7,19 +8,19 @@ class UserHelper:
     # Create New User Object
     @staticmethod
     def new_user(currentuser):
-        with open(currentuser["username"]+".txt", "w") as outfile:
+        with open(settings.config.storage["path"] + currentuser["username"]+".txt", "w") as outfile:
             json.dump(currentuser, outfile)
         outfile.close()
 
     # Delete passed User UUID
     @staticmethod
     def delete_user(username):
-        os.remove(username + ".txt")
+        os.remove(settings.config.storage["path"] + username + ".txt")
 
     # Get a user using username and returns user dictionary
     @staticmethod
     def get_user(username):
-        with open(username + ".txt") as file:
+        with open(settings.config.storage["path"] + username + ".txt") as file:
             user = json.load(file)
         file.close()
         return user
@@ -27,6 +28,6 @@ class UserHelper:
     # Update User Objects
     @staticmethod
     def update_user(currentuser):
-        with open(currentuser["username"]+".txt", "w") as outfile:
+        with open(settings.config.storage["path"] + currentuser["username"]+".txt", "w") as outfile:
             json.dump(currentuser, outfile)
         outfile.close()
