88944550027ac3b74991aa4106ea1a9d7860fd16
Justin Scott <justinjonscott@gmail.com>
2019-02-06 14:02:21 -0500

    settings for other OS

--- settings/config.py
+++ settings/config.py
@@ -1,7 +1,22 @@
-DEFAULT_LOCAL_PATH = "%appdata%/VisualSP2019/Users/"
+from sys import platform
+
+DEFAULT_LOCAL_PATH_WINDOWS = "%appdata%/VisualSP2019/Users/"
+DEFAULT_LOCAL_PATH_MAC = "~/Library/Preferences"
+DEFAULT_LOCAL_PATH_LINUX =  "~/.visualsp"
+
+
+osType = "W"
+if platform == "linux":
+    osType = "L"
+elif platform == "darwin":
+    osType == "M"
+elif platform == "win32" or platform == "win64":
+    osType = "W"
 
 storage = dict(
-    path = DEFAULT_LOCAL_PATH,
+    pathW = DEFAULT_LOCAL_PATH_WINDOWS,
+    pathM = DEFAULT_LOCAL_PATH_MAC,
+    pathL = DEFAULT_LOCAL_PATH_LINUX,
 )
 
 
--- user/UserHelper.py
+++ user/UserHelper.py
@@ -8,19 +8,19 @@ class UserHelper:
     # Create New User Object
     @staticmethod
     def new_user(currentuser):
-        with open(settings.config.storage["path"] + currentuser["username"]+".txt", "w") as outfile:
+        with open(settings.config.storage["path" + settings.config.osType] + currentuser["username"]+".txt", "w") as outfile:
             json.dump(currentuser, outfile)
         outfile.close()
 
     # Delete passed User UUID
     @staticmethod
     def delete_user(username):
-        os.remove(settings.config.storage["path"] + username + ".txt")
+        os.remove(settings.config.storage["path" + settings.config.osType] + username + ".txt")
 
     # Get a user using username and returns user dictionary
     @staticmethod
     def get_user(username):
-        with open(settings.config.storage["path"] + username + ".txt") as file:
+        with open(settings.config.storage["path" + settings.config.osType] + username + ".txt") as file:
             user = json.load(file)
         file.close()
         return user
@@ -28,6 +28,6 @@ class UserHelper:
     # Update User Objects
     @staticmethod
     def update_user(currentuser):
-        with open(settings.config.storage["path"] + currentuser["username"]+".txt", "w") as outfile:
+        with open(settings.config.storage["path" + settings.config.osType] + currentuser["username"]+".txt", "w") as outfile:
             json.dump(currentuser, outfile)
         outfile.close()
