9fe04603b3cae72cc87c5d6a36e8d5c04050786f
Justin Scott <justinjonscott@gmail.com>
2019-02-26 11:51:19 -0500

    Update config.py
    added default path if OS undetermined.

--- settings/config.py
+++ settings/config.py
@@ -1,18 +1,20 @@
 from sys import platform
 
 DEFAULT_LOCAL_PATH_WINDOWS = "%appdata%/VisualSP2019/Users/"
-DEFAULT_LOCAL_PATH_MAC = "~/Library/Preferences"
-DEFAULT_LOCAL_PATH_LINUX = "~/.visualsp"
+DEFAULT_LOCAL_PATH_MAC = "~/Library/Preferences/visualsp/Users/"
+DEFAULT_LOCAL_PATH_LINUX = "~/.visualsp/users/"
+DEFAULT_LOCAL_PATH_DEFAULT = "/Users/"
 
 DEFAULT_ACCESS_LEVEL = "0"
 GENERAL_ACCESS_LEVEL = "0"
 ADMIN_ACCESS_LEVEL = "10"
 
-osType = "W"
+osType = "Default"
 storage = dict(
-    pathW=DEFAULT_LOCAL_PATH_WINDOWS,
-    pathM=DEFAULT_LOCAL_PATH_MAC,
-    pathL=DEFAULT_LOCAL_PATH_LINUX,
+    pathW = DEFAULT_LOCAL_PATH_WINDOWS,
+    pathM = DEFAULT_LOCAL_PATH_MAC,
+    pathL = DEFAULT_LOCAL_PATH_LINUX,
+    pathDefault = DEFAULT_LOCAL_PATH_DEFAULT
 )
 
 
@@ -23,3 +25,4 @@ def __init__(self):
         self.osType == "M"
     elif platform == "win32" or platform == "win64":
         self.osType = "W"
+
